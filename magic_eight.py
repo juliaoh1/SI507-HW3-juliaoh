8e7de83b7bc1ab1da0da0677cc0ee6ab887da2e4
Zimeng Ding <dingzimeng96@yahoo.com>
2019-09-18 15:02:43 -0400

    delete

--- magic_eight.py
+++ magic_eight.py
@@ -20,11 +20,3 @@ def askQuestion():
     return quest
 
 quest = askQuestion()
-
-def pickAnswer(answers_list):
-    i = random.choice(answers_list)
-    print(i)
-    return(i)
-
-answers_list = ["It is certain.", "It is decidedly so.", "Without a doubt.","Yes - definitely.","You may rely on it.","As I see it, yes."," Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
-pickAnswer(answers_list)
