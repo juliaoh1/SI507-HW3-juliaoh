8c5c9decb769aff364f7f3be7022fb78f5ce499b
juliaoh1 <juliaoh@umich.edu>
2019-09-19 11:43:37 -0400

    finalizing changes to check_questions

--- magic_eight.py
+++ magic_eight.py
@@ -27,14 +27,19 @@ def pickAnswer(answers_list):
     return(i)
 
 answers_list = ["It is certain.", "It is decidedly so.", "Without a doubt.","Yes - definitely.","You may rely on it.","As I see it, yes."," Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
-pickAnswer(answers_list)
+#pickAnswer(answers_list)
 
 def testQuestion(quest):
     
     if quest[-1] == "?":
-        returnResponse()
+        pickAnswer(answers_list)
     else:
         print ("I'm sorry I can only answer questions.")
 
 #keeps asking for user input until the user types quit
+#quest = askQuestion()       
 testQuestion(quest)
+
+while quest!="quit":
+    quest = askQuestion()
+    testQuestion(quest)
